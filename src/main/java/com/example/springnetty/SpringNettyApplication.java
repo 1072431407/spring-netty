6a88c9caae7264671e3c9da6b963dcf123a782d5
zyj-notebook <1072431407@qq.com>
2020-08-31 14:34:21 +0800

    init

---
+++ src/main/java/com/example/springnetty/SpringNettyApplication.java
@@ -0,0 +1,16 @@
+package com.example.springnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://www.jb51.net/article/153546.htm
+ */
+@SpringBootApplication
+public class SpringNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNettyApplication.class, args);
+    }
+
+}
